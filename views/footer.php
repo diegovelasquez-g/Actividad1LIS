a7db1d8df23f31cb61a68616cf6e0cfe36b33be8
diegovelasquez-g <velasquez.alejandrod@gmail.com>
2022-02-14 07:52:19 -0600

    CRUD MVC PHP

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col text-center">
+			<p>Diego Alejandro Velásquez Gómez VG190501 y Alejandro José Castillo Pacheco CP190719 </p>
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="./misc/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="./misc/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="./js/users/users.js"></script>
+<script type="text/javascript">
+	objUser=new Users();
+</script>
+</body>
+</html>
---
+++ views/users/toolbar.php
@@ -0,0 +1,12 @@
+<div class="row mt-2">
+	<div class="col-1">
+		<a href="./index.php?page=users&folder=users" title="Usuarios">
+			<button class="btn btn-primary">Listado</button>
+		</a>
+	</div>
+	<div class="">
+		<a href="./index.php?page=new&folder=users" title="Nuevo">
+			<button class="btn btn-success">Agregar</button>
+		</a>
+	</div>
+</div>
